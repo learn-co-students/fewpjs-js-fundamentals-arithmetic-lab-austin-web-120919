c1401629b172351610f873bfa68c4de860243c67
Chance Mims <chance@Chances-MacBook-Air.local>
2020-02-03 11:10:08 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+function multiplyNums(num1, num2){
+    return (num1 * num2);
+}
+
+let multiply = multiplyNums(num1, num2); 
+
+
+
+function randomize(){
+    return (Math.floor(Math.random() * 100 ) +1);
+}
+
+let random = randomize();
+
+function maximize(numSet){
+    return Math.max(numSet);
+}
+
+let max = maximize(20);
+
+function modulate(num1, num2){
+   return num1 % num2;
+}
+
+let mod = modulate(4, 5);
